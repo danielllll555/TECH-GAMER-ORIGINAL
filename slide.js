94d1fa5d1e0679b66b7605ea1ced6f5cc289518d
Julio Menezes <149852894+JuMnzs@users.noreply.github.com>
2024-05-23 21:05:03 -0300

    Add files via upload

--- slide.js
+++ slide.js
@@ -78,15 +78,39 @@ var total = 0;
 // Mapeamento dos produtos
 var produtos = {
   1: { nome: "Processador AMD Ryzen 5 4600G", preco: 628.22, img: "ryzen.jpg" },
-  2: { nome: "Processador AMD Ryzen 5 3600G", preco: 589.0, img: "Ryzen.2.jpg" },
+  2: {
+    nome: "Processador AMD Ryzen 5 3600G",
+    preco: 589.0,
+    img: "Ryzen.2.jpg",
+  },
   3: { nome: "Processador AMD Ryzen 5 5600G", preco: 919.99, img: "ryzen.jpg" },
-  4: { nome: "Processador Intel Core i3-12100", preco: 799.99, img: "intel 2.jpeg" },
-  5: { nome: "Processador Intel Core i3-12100F", preco: 588.99, img: "intel.jpeg" },
+  4: {
+    nome: "Processador Intel Core i3-12100",
+    preco: 799.99,
+    img: "intel 2.jpeg",
+  },
+  5: {
+    nome: "Processador Intel Core i3-12100F",
+    preco: 588.99,
+    img: "intel.jpeg",
+  },
   6: { nome: "Mouse Gamer Pichau Hive P1", preco: 129.99, img: "mouse.jpg" },
-  7: { nome: "Teclado Mecanico Gamer Redragon Daksa", preco: 259.99, img: "teclado.jpg" },
+  7: {
+    nome: "Teclado Mecanico Gamer Redragon Daksa",
+    preco: 259.99,
+    img: "teclado.jpg",
+  },
   8: { nome: "Monitor Gamer Asus Tuf", preco: 899.99, img: "monitor.jpg" },
-  9: { nome: "Cadeira Gamer Pichau Omega S", preco: 1705.99, img: "cadeira.jpg" },
-  10: { nome: "Gabinete Cooler Master Masterbox MB530P", preco: 679.99, img: "gabinete 5.jpg" }
+  9: {
+    nome: "Cadeira Gamer Pichau Omega S",
+    preco: 1705.99,
+    img: "cadeira.jpg",
+  },
+  10: {
+    nome: "Gabinete Cooler Master Masterbox MB530P",
+    preco: 679.99,
+    img: "gabinete 5.jpg",
+  },
 };
 
 function adicionarProduto(id) {
@@ -124,22 +148,29 @@ function atualizarResumoCarrinho() {
     listaProdutos.appendChild(li);
   }
 
-  document.getElementById("totalCarrinho").innerText = "Total: R$ " + total.toFixed(2);
+  document.getElementById("totalCarrinho").innerText =
+    "Total: R$ " + total.toFixed(2);
 }
 
-document.getElementById("linkCarrinho").addEventListener("click", function(event) {
-  event.preventDefault();
-  var resumoCarrinho = document.getElementById("resumoCarrinho");
-  resumoCarrinho.style.display = resumoCarrinho.style.display === "none" ? "block" : "none";
-});
+document
+  .getElementById("linkCarrinho")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    var resumoCarrinho = document.getElementById("resumoCarrinho");
+    resumoCarrinho.style.display =
+      resumoCarrinho.style.display === "none" ? "block" : "none";
+  });
 
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
   var resumoCarrinho = document.getElementById("resumoCarrinho");
-  if (!resumoCarrinho.contains(event.target) && event.target.id !== "linkCarrinho") {
+  if (
+    !resumoCarrinho.contains(event.target) &&
+    event.target.id !== "linkCarrinho"
+  ) {
     resumoCarrinho.style.display = "none";
   }
 });
 
-document.getElementById("irPagamento").addEventListener("click", function() {
-  window.location.href = "pagamento.html";
+document.getElementById("irPagamento").addEventListener("click", function () {
+  window.location.href = "cartao.html";
 });
